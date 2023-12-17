36a54fe04b6ea7f016208332059c0fe6308dc128
Leen151 <leen.m@orange.fr>
2023-12-17 16:40:18 +0100

    Importation des datas + intégration page d'accueil partie 1

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,37 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.classList.add("photographer");
+        
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const localisation = document.createElement('h3');
+        localisation.classList.add("localisation");
+        localisation.textContent = city + ", " + country;
+
+        const description = document.createElement('p');
+        description.classList.add("description");
+        description.textContent = tagline;
+
+        const prix = document.createElement('p');
+        prix.classList.add("price");        
+        prix.textContent = String(price) + "€/jour";
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(localisation);
+        article.appendChild(description);
+        article.appendChild(prix);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, id, picture, getUserCardDOM }
 }
